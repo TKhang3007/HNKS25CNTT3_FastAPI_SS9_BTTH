0de56637e1ad6d6e0474b5d103fe48d891781984
TKhang3007 <d.khang3007@gmail.com>
2026-07-06 21:55:56 +0700

    Add files via upload

---
+++ btth.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI, status, HTTPException, Depends
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker, Session
+from pydantic import BaseModel
+
+DATABASE_URL = "mysql+pymysql://root:khang30072007@localhost:3306/ecommerce_db"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+class ShipmentModel(Base):
+    __tablename__ = "shipments"
+
+    id = Column(Integer, primary_key=True)
+    tracking_number = Column(String(50), unique=True, nullable=False)
+    status = Column(String(50), default="PREPARING")
+
+Base.metadata.create_all(bind=engine)
+
+class ShipmentCreate(BaseModel):
+    tracking_number: str
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/shipments", status_code=status.HTTP_201_CREATED)
+def create_shipment(shipment: ShipmentCreate,db: Session = Depends(get_db)):
+
+    existing = db.query(ShipmentModel).filter(ShipmentModel.tracking_number == shipment.tracking_number).first()
+
+    if existing:
+        raise HTTPException(
+            status_code=400,
+            detail="Mã vận đơn này đã được khởi tạo trước đó"
+        )
+    
+    new_shipment = ShipmentModel(tracking_number=shipment.tracking_number)
+
+    db.add(new_shipment)
+    db.commit()
+    db.refresh(new_shipment)
+
+    return {
+        "message": "Tạo vận đơn thành công",
+        "data": new_shipment
+    }
